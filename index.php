2bf14dd49081bc7fe96ea6caa28619386ff0fb68
tobychui <chuicheuklong@hotmail.com>
2016-12-17 12:52:53 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+<meta charset="UTF-8"> 
+<link rel="stylesheet" href="//cdn.rawgit.com/TeaMeow/TocasUI/master/dist/tocas.min.css">
+<title>ArOZ System</title>
+</head>
+<body>
+<br>
+<div class="ts container">
+	<h1>IMUS Laboratory</h1><br>
+	<h2>ArOZ Web Interface</h2>
+<div class="ts pointing secondary big menu">
+    <a class="active item">Index</a>
+    <a class="item" href="music.php">Music</a>
+	<a class="item" href="video.php">Video</a>
+	<a class="item" href="upload/">Upload</a>
+	<a class="item" href="upload/uploads/">Upload Dir</a>
+</div>
+
+<div class="ts very narrow container">
+<img class="ts fluid image" src="image/Banner.png">
+</div>
+<div class="ts text container">
+<div class="ts slate">
+    <div class="icons">
+        <i class="Music outline icon"></i>
+        <i class="Record outline icon"></i>
+        <i class="Upload outline icon"></i>
+    </div>
+    <span class="header">ArOZ Online</span>
+    <span class="description">Thanks for using ArOZ Web based multimedia streaming package.</span>
+	<span class="description">Congratulation, you have successfully installed ArOZ System.</span>
+</div>
+<div class="ts basic padded slate">
+    <i class="Check Circle Outline symbol icon"></i>
+    <span class="header">Tips</span>
+    <span class="description">- You can upload video format files to Music Bank on the upload page.
+	<br>- Multi-language filename upload support!
+	<br>- Support only mp3 and mp4, but you can always edit the code to support more formats.
+	<br>- No Database needed, fully stand alone system.</span>
+</div>
+</div>
+<div class="ts divider"></div>
+<div class="ts grid">
+	<div class="two column row">
+        <div class="column"><p>CopyRight  ArOZ System Omega, ArOZ Project, IMUS Laboratory 2016 </p>
+		</div>
+        <div class="column"><p align="right"><img class="ts small image" src="logo.png""></p></div>
+    </div>
+    </div>
+</div>
+</body>
+
+
+</html>
